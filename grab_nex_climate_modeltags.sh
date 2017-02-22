26ed28df4c619b833ae630146ab3963887dbc257
jennabaughman <jbaughman@berkeley.edu>
2017-02-22 10:03:47 -0800

    adding explanation/documentation comments to this script

--- grab_nex_climate_modeltags.sh
+++ grab_nex_climate_modeltags.sh
@@ -1,4 +1,9 @@
 # grab the lines that start with mkdir
+# pipe only the lines that have '/historical/', to reduce duplicates from other scenarios,
+# replace everything before '/historical/' with blank (delete it)
+# replace '/pr/', '/tasmin/', and '/tasmax/' with blank (delete them)
+# all that's left is the model names
+# remove duplicates; keep only one copy of each model name in list
 
 grep '^mkdir' make_directories_files.sh | grep '/historical/' | sed -e 's:^.*/historical/::' | sed -e 's:/pr/.*::' | sed -e 's:/tasmin/.*::' | sed -e 's:/tasmax/.*::' | uniq
 
