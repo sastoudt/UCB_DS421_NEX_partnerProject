11ac9ae47564967e197957acb18bd5d3aca8bed5
sastoudt <sstoudt@berkeley.edu>
2017-03-11 17:19:01 -0800

    setup automatic code generation for loading and naming images for d3 slider

--- loadImgD3Slider.R
+++ loadImgD3Slider.R
@@ -18,10 +18,25 @@ histOneTag=oneTag[which(grepl("historical",oneTag))]
 
 histMaxTempOneTag=histOneTag[which(grepl("tasmax",histOneTag))]
 
-yr=seq(1950,2005,by=1)
-writeLines(paste("<img id=\"img-",yr,"\" class=\"bottom\" src=\"",yr,".png\" />",sep=""))
+yr1=seq(1950,2006,by=1/365)
+yr1=round(yr1*1000)/1000
+
+yr=floor(yr1)
+yr=yr[-length(yr)]
+
+#table(yr)
+
+#yr=sort(rep(seq(1950,2005,by=1),365))
+day=rep(1:365,56)
+toWrite=paste("<img id=\"img-",yr1,"\" class=\"bottom\" src=\"","pr_day_BCSD_historical_r1i1p1_MIROC-ESM-CHEM_",
+              yr,".ncday",day,".png\" />",sep="")
+length(toWrite)
+toWrite=toWrite[-length(toWrite)]
+writeLines(toWrite)
 
 ## change class of 1950 to top
 ## copy and paste into index.html
 
 ## but what will the figures be called, png v. svg?
+
+#pr_day_BCSD_historical_r1i1p1_MIROC-ESM-CHEM_1950.ncday1.png
