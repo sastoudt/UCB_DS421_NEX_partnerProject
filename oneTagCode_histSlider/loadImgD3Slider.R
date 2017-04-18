0b9f87a5f138e6fce6a92e17c9a06229140df1ab
sastoudt <sstoudt@berkeley.edu>
2017-04-17 21:05:06 -0700

    allow for slider to have dropdown for different variable/summary combinations

--- oneTagCode_histSlider/loadImgD3Slider.R
+++ oneTagCode_histSlider/loadImgD3Slider.R
@@ -11,36 +11,36 @@ histMaxTempOneTag=histOneTag[which(grepl("tasmax",histOneTag))]
 
 yr1=seq(1950,2005,by=1)
 
-toWrite=paste("<img id=\"img-",yr1,"\" class=\"bottom\" src=\"","tasmax_day_BCSD_historical_r1i1p1_MIROC-ESM-CHEM_",
+toWrite=paste("<img id=\"img-maxTempMean",yr1,"\" class=\"bottom\" src=\"","tasmax_day_BCSD_historical_r1i1p1_MIROC-ESM-CHEM_",
               yr1,".ncmean",".png\" />",sep="")
 length(toWrite)
 writeLines(toWrite)
 
-## change class of 1950 to top
+## change class of 1950 to top (default to show first in app)
 ## copy and paste into index.html
 
 histMinTempOneTag=histOneTag[which(grepl("tasmin",histOneTag))]
 
 yr1=seq(1950,2005,by=1)
 
-toWrite=paste("<img id=\"img-",yr1,"\" class=\"bottom\" src=\"","tasmin_day_BCSD_historical_r1i1p1_MIROC-ESM-CHEM_",
+toWrite=paste("<img id=\"img-minTempMean",yr1,"\" class=\"bottom\" src=\"","tasmin_day_BCSD_historical_r1i1p1_MIROC-ESM-CHEM_",
               yr1,".ncmean",".png\" />",sep="")
 length(toWrite)
 writeLines(toWrite)
 
-## change class of 1950 to top
+
 ## copy and paste into index.html
 
 histPrTempOneTag=histOneTag[which(grepl("pr",histOneTag))]
 
 yr1=seq(1950,2005,by=1)
 
-toWrite=paste("<img id=\"img-",yr1,"\" class=\"bottom\" src=\"","pr_day_BCSD_historical_r1i1p1_MIROC-ESM-CHEM_",
+toWrite=paste("<img id=\"img-prMean",yr1,"\" class=\"bottom\" src=\"","pr_day_BCSD_historical_r1i1p1_MIROC-ESM-CHEM_",
               yr1,".ncmean",".png\" />",sep="")
 length(toWrite)
 writeLines(toWrite)
 
-## change class of 1950 to top
+
 ## copy and paste into index.html
 
 
@@ -57,36 +57,34 @@ histMaxTempOneTag=histOneTag[which(grepl("tasmax",histOneTag))]
 
 yr1=seq(1950,2005,by=1)
 
-toWrite=paste("<img id=\"img-",yr1,"\" class=\"bottom\" src=\"","tasmax_day_BCSD_historical_r1i1p1_MIROC-ESM-CHEM_",
+toWrite=paste("<img id=\"img-maxTempSD",yr1,"\" class=\"bottom\" src=\"","tasmax_day_BCSD_historical_r1i1p1_MIROC-ESM-CHEM_",
               yr1,".ncsd",".png\" />",sep="")
 length(toWrite)
 writeLines(toWrite)
 
-## change class of 1950 to top
+
 ## copy and paste into index.html
 
 histMinTempOneTag=histOneTag[which(grepl("tasmin",histOneTag))]
 
 yr1=seq(1950,2005,by=1)
 
-toWrite=paste("<img id=\"img-",yr1,"\" class=\"bottom\" src=\"","tasmin_day_BCSD_historical_r1i1p1_MIROC-ESM-CHEM_",
+toWrite=paste("<img id=\"img-minTempSD",yr1,"\" class=\"bottom\" src=\"","tasmin_day_BCSD_historical_r1i1p1_MIROC-ESM-CHEM_",
               yr1,".ncsd",".png\" />",sep="")
 length(toWrite)
 writeLines(toWrite)
 
-## change class of 1950 to top
 ## copy and paste into index.html
 
 histPrTempOneTag=histOneTag[which(grepl("pr",histOneTag))]
 
 yr1=seq(1950,2005,by=1)
 
-toWrite=paste("<img id=\"img-",yr1,"\" class=\"bottom\" src=\"","pr_day_BCSD_historical_r1i1p1_MIROC-ESM-CHEM_",
+toWrite=paste("<img id=\"img-prSD",yr1,"\" class=\"bottom\" src=\"","pr_day_BCSD_historical_r1i1p1_MIROC-ESM-CHEM_",
               yr1,".ncsd",".png\" />",sep="")
 length(toWrite)
 writeLines(toWrite)
 
-## change class of 1950 to top
 ## copy and paste into index.html
 
 
