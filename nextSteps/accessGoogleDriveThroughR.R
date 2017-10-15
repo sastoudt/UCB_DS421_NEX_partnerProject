c7ccbf9211076b245573ec3d4163bb594df023b1
sastoudt <sstoudt@berkeley.edu>
2017-10-14 17:20:21 -0700

    add place to put new code, start to googledrive tidyverse workflow

---
+++ nextSteps/accessGoogleDriveThroughR.R
@@ -0,0 +1,7 @@
+### sample workflow to upload, download, and change sharing permission for Google Drive storage through R
+
+# http://googledrive.tidyverse.org/
+
+##install.packages("googledrive")
+
+library("googledrive")
