27072394f391f5a8bddd8e48e2ded3b8ec90244c
sastoudt <sstoudt@berkeley.edu>
2017-10-28 10:28:27 -0700

    add sharing and getting data, need to figure out how to automate keys

--- nextSteps/accessGoogleDriveThroughR.R
+++ nextSteps/accessGoogleDriveThroughR.R
@@ -1,15 +1,20 @@
 ### sample workflow to upload, download, and change sharing permission for Google Drive storage through R
 
+#### Packages ####
 # http://googledrive.tidyverse.org/
 
 install.packages("googledrive")
+#install.packages("googlesheet")
+devtools::install_github("jennybc/googlesheets")
 
 library("googledrive")
+library("googlesheets")
 
-
+#### Generate Fake Data as Test ####
 fakedata=cbind(rnorm(100),rnorm(100),rnorm(100))
 write.csv(fakedata,"fakedata.csv",row.names=F)
-### upload files
+
+#### upload files ####
 
 testData=drive_upload("fakedata.csv",path="DS421capstone/",name="testData.csv")
 
@@ -20,11 +25,30 @@ testData=drive_upload("fakedata.csv",path="DS421capstone/",name="testData.csv")
 # 
 # Selection: 1
 
-### In our case, the data we would be uploading would be the restructured files (time series per grid cell)
+## In our case, the data we would be uploading would be the restructured files (time series per grid cell)
+
+ 
+#### giving access to files/folders ####
+
+testData=drive_get("DS421capstone/testData.csv") ## not actual data, just file object
 
 
-#### giving access to files/folders
+testData %>% 
+  drive_reveal("permissions") ## add permissions field
 
+testData <- testData %>%
+    drive_share(role = "reader", type = "anyone") ## let anyone with the link read the data
 
-#### grabbing data 
+#### how to get key/url ####
+
+#?
+
+## need to create some kind of look up table of keys for each grid cell
+## given lat long or bounding box, give keys to access our processed data
+
+#### grabbing data  ####
+
+someoneGetData <- testData$id %>%
+  gs_key()
+
 
